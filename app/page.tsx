92dc149854b8259215c0c849f1ae2c80ad1c3c47
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2024-07-20 12:19:14 +0900

    Fix: icon

--- app/page.tsx
+++ app/page.tsx
@@ -77,10 +77,25 @@ export default function Home() {
             <div className="">
               <NFT />
             </div>
-            <Avatar
-              src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-              fallback="A"
-            />
+            <div>
+              <Avatar src="/Logos/ts-logo-128.png" fallback="A" />
+              <Avatar
+                src="/Logos/solidity.svg"
+                fallback="A"
+                className="bg-white"
+              />
+              <Avatar
+                src="/Logos/Go-Logo.png"
+                fallback="A"
+                className="bg-white"
+              />
+              <Avatar src="/Logos/c.png" fallback="A" className="bg-white" />
+              <Avatar
+                src="/Logos/react-logo.svg"
+                fallback="A"
+                className="bg-white"
+              />
+            </div>
           </div>
         </div>
       </main>
