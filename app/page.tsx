b23fc63d708bc106a5da37501038421389268f24
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2024-07-22 02:32:43 +0900

    Fix: 全体を加工レイアウトを修正

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ import "@radix-ui/themes/styles.css";
 import { Avatar, Card, Inset, Text, Theme } from "@radix-ui/themes";
 import Image from "next/image";
 import { SkilBox } from "@/components/SkilBox";
+import { IMG } from "@/components/IMG";
 
 export default function Home() {
   const [showText, setShowText] = useState(false);
@@ -59,10 +60,11 @@ export default function Home() {
 
   return (
     <Theme accentColor="blue">
-      <main className="h-screen w-full bg-gradient-to-r from-[#e0eaf3] to-[#cfdef3] pt-5">
-        {/* <Header /> */}
+      <main className="h-screen w-full bg-gradient-to-r from-[#e0eaf3] to-[#cfdef3]">
+        <div className="mx-auto flex max-w-screen-xl gap-20 px-5 py-10">
+          {/* <Header /> */}
 
-        {/* <header className="fixed top-0 h-screen w-full overflow-hidden">
+          {/* <header className="fixed top-0 h-screen w-full overflow-hidden">
         <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -90,16 +92,32 @@ export default function Home() {
             )}
             </motion.div>
             </header> */}
+          <nav className="w-20 rounded-lg bg-gray-500/40"></nav>
 
-        <div className="mx-auto max-w-5xl px-4">
           <div className="grid grid-cols-2 grid-rows-2 place-items-start gap-5">
+            <div className="">
+              <h2 className="text-7xl font-black">
+                Welcome to <br />
+                My portfolio
+              </h2>
+              <div className="mt-5 flex items-end">
+                <IMG
+                  src="/NFTs/aa.jpg"
+                  className="aspect-square w-40 rounded-full"
+                />
+                <div>
+                  <h3 className="text-3xl font-semibold">Nonaka Dai</h3>
+                  <p className="font-bold">Utsunomiya University b3</p>
+                </div>
+              </div>
+            </div>
             <div className="grade-bg rounded-2xl p-5">
               <Modal />
             </div>
-            <div className="mx-auto">
+            <div className="">
               <NFT />
             </div>
-            <div className="flex h-full items-center">
+            <div className="">
               <div>
                 <SkilBox data={front}>
                   使用頻度が高い技術スタックです。
--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,62 +1,99 @@
-const projects = [
-  {
-    id: "sm",
-    compornent: <SmartPocket />,
-    src: "/sources/sm_logo.png",
-    color: "",
-    size: 140,
-  },
-  {
-    id: "ds",
-    compornent: <Dayshare />,
-    src: "/sources/ds_logo.png",
-    color: "",
-    size: 80,
-  },
-  {
-    id: "lb",
-    compornent: <LECBLO />,
-    src: "/sources/lb_logo.png",
-    color: "",
-    size: 100,
-    custom: "RECBLO",
-  },
-];
-
 import * as Dialog from "@radix-ui/react-dialog";
 import { SmartPocket } from "../Slide/SmartPocket";
 import { Dayshare } from "../Slide/Dayshare";
 import { LECBLO } from "../Slide/LECBLO";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { IoLogoGithub } from "react-icons/io";
+import { Button } from "@radix-ui/themes";
+import { FaLink } from "react-icons/fa";
+
+const projects = [
+  {
+    id: "sm",
+    compornent: <SmartPocket />,
+    src: "/sources/sm_logo.png",
+    size: 140,
+    link: {
+      site: "https://www.smapocke.app/",
+    },
+  },
+  {
+    id: "ds",
+    compornent: <Dayshare />,
+    src: "/sources/ds_logo.png",
+    size: 80,
+    link: { github: "https://github.com/nanashi060/dayshare" },
+  },
+  {
+    id: "lb",
+    compornent: <LECBLO />,
+    src: "/sources/lb_logo.png",
+    size: 100,
+    link: { github: "https://github.com/taisei08/reco_blo" },
+    custom: "RECBLO",
+  },
+];
 
 export const Modal = () => {
   const [selected, setSelected] = useState("");
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <div className="mx-auto grid w-[350px] grid-cols-2 gap-5">
+        <div className="mx-auto grid grid-cols-2 gap-5">
           {projects.map((project) => (
-            <button
-              key={project.id}
-              className={`${project.color} h-28 w-40 rounded-xl bg-white/60 shadow-lg transition-transform hover:-translate-y-[2px] hover:shadow-xl`}
-              onClick={() => setSelected(project.id)}
-            >
-              <div className="flex justify-center">
-                {project.custom && (
-                  <div className="absolute mt-3 text-2xl font-bold text-white">
-                    {project.custom}
+            <div key={project.id}>
+              <button
+                key={project.id}
+                className={`h-28 w-40 rounded-xl bg-white/60 shadow-lg transition-transform hover:-translate-y-[2px] hover:shadow-xl`}
+                onClick={() => setSelected(project.id)}
+              >
+                <div className="flex justify-center">
+                  {project.custom && (
+                    <div className="absolute mt-3 text-2xl font-bold text-white">
+                      {project.custom}
+                    </div>
+                  )}
+                  <Image
+                    src={project.src}
+                    height={project.size}
+                    width={project.size}
+                    alt=""
+                    style={{ objectFit: "cover" }}
+                  />
+                </div>
+              </button>
+              {project.link && (
+                <div className="mt-2">
+                  <div className="flex flex-wrap gap-1">
+                    {project.link.site && (
+                      <Button size="1" variant="solid" color="cyan">
+                        <Link
+                          href={project.link.site}
+                          className="flex items-center gap-1"
+                          target="_blank"
+                        >
+                          <FaLink />
+                          <a className="">Site</a>
+                        </Link>
+                      </Button>
+                    )}
+                    {project.link.github && (
+                      <Button size="1" variant="solid" color="gray">
+                        <Link
+                          href={project.link.github}
+                          className="flex items-center gap-1"
+                        >
+                          <IoLogoGithub />
+                          <a className="">Github</a>
+                        </Link>
+                      </Button>
+                    )}
                   </div>
-                )}
-                <Image
-                  src={project.src}
-                  height={project.size}
-                  width={project.size}
-                  alt=""
-                  style={{ objectFit: "cover" }}
-                />
-              </div>
-            </button>
+                </div>
+              )}
+            </div>
           ))}
         </div>
       </Dialog.Trigger>
