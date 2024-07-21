2a333b887471dae33f8c66d3e34e96692b2fe90c
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2024-07-22 01:08:33 +0900

    Fix: modalのエラーを解消

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -63,6 +63,7 @@ export const Modal = () => {
       <Dialog.Portal>
         <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black/50" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium"></Dialog.Title>
           {projects.map((project) => (
             <div key={project.id}>
               {selected === project.id && project.compornent}
