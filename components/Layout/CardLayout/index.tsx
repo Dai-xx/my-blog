81d4f6c2c13a9e0d88633715e6361202acbc8a1b
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2024-07-21 17:25:36 +0900

    Fix: スライド内容のコンポーネント化

---
+++ components/Layout/CardLayout/index.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { FC } from "react";
+
+type Props = {
+  data: {
+    src?: string;
+    icon?: React.ReactNode;
+    title: string;
+    content?: React.ReactNode;
+    text?: string;
+  }[];
+};
+
+export const CardLayout: FC<Props> = ({ data }) => {
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 rounded-lg">
+      {data.map((item, index) => (
+        <section
+          key={index}
+          className="flex h-full w-full flex-col items-center rounded-xl border-2 border-gray-300 p-2 shadow-lg"
+        >
+          {item.src && (
+            <div className="relative flex aspect-square w-[100px] items-center justify-center">
+              <Image
+                src={item.src}
+                alt=""
+                height={100}
+                width={100}
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          )}
+
+          {item.icon && <>{item.icon}</>}
+          <p className="text-center text-3xl font-extrabold">{item.title}</p>
+          {item.text && <p className="mt-3">{item.text}</p>}
+          {item.content && <>{item.content}</>}
+        </section>
+      ))}
+    </div>
+  );
+};
--- components/Slide/SmartPocket/index.tsx
+++ components/Slide/SmartPocket/index.tsx
@@ -1,21 +1,22 @@
 import { FC } from "react";
 import Image from "next/image";
 import { Slide } from "@/components/Layout/Slide";
-import { Blockquote, Box, Card, Code, Theme } from "@radix-ui/themes";
-import Link from "next/link";
+import { Code, Theme } from "@radix-ui/themes";
 import { Board } from "@/components/Layout/Board";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { FiDatabase } from "react-icons/fi";
 import { FaEthereum } from "react-icons/fa";
+import { BoardInner } from "@/components/Layout/BoardInner";
+import { CardLayout } from "@/components/Layout/CardLayout";
 
-const data = [
+const data_3 = [
   {
     icon: <MdOutlineDesignServices size={100} color="#FF9900" />,
     title: "Design",
     text: "デザインカンプを元に、コーディングを行います。",
   },
   {
-    icon: <FiDatabase size={100} color="#0099FF" />,
+    icon: <FiDatabase size={100} color="#009900" />,
     title: "Database",
     text: "Firestoreを使用して、データベース情報を取得します。",
   },
@@ -50,7 +51,7 @@ const data_4 = [
   {
     src: "/sources/cat.svg",
     title: "Tech",
-    text: (
+    content: (
       <div className="flex flex-wrap gap-1">
         {data_tech.map((item: string, index) => (
           <Code key={index}>{item}</Code>
@@ -65,26 +66,8 @@ export const SmartPocket: FC = () => {
     <Board>
       <Slide
         slides={[
-          <div key="slide1">
+          <BoardInner key="slide1">
             <div className="flex justify-between">
-              {/* 1つ目のスライドの内容 */}
-
-              {/* <div className="flex h-full flex-col justify-start">
-              <Theme>
-              <Blockquote size="8">デザインの実装・修正</Blockquote>
-              </Theme>
-              
-              <p className="mt-5">
-              デザインカンプを元に、コーディングを行います。
-              </p>
-              <p className="mt-5">
-              月２のペースでデザイナーと通話でデザイン修正会を行います。
-              </p>
-              <p className="mt-5">
-              Tailwindcss, radix-ui, react-spring
-              などモダンなデザインライブラリを使用しています。
-              </p>
-              </div> */}
               <div className="relative h-[469px] w-[300px] rounded-t-xl border-x-2 border-t-2">
                 <Image
                   src="/sources/sm_01.jpg"
@@ -110,22 +93,10 @@ export const SmartPocket: FC = () => {
                 />
               </div>
             </div>
-          </div>,
-          <div key="slide2">
-            {/* 2つ目のスライドの内容 */}
+          </BoardInner>,
+
+          <BoardInner key="slide2">
             <div className="flex justify-between">
-              {/* <div className="flex h-full flex-col justify-start">
-                  <Theme>
-                  <Blockquote size="8">BC情報の連携</Blockquote>
-                  </Theme>
-                  <p className="mt-5">
-                  お客様のブロックチェーン情報を取得し、自動でデータを更新します。
-                  </p>
-                  <p className="mt-5">alchemyのapiを使用しています。</p>
-                  <p className="mt-5">
-                  スマコン開発経験があり、ブロックチェーンの基礎的な知識があります。
-                  </p>
-                  </div> */}
               <div className="relative h-[469px] w-[300px] rounded-t-xl border-x-2 border-t-2">
                 <Image
                   src="/sources/sm_03.png"
@@ -151,73 +122,17 @@ export const SmartPocket: FC = () => {
                 />
               </div>
             </div>
-          </div>,
-          <div key="slide3">
-            {/* 3つ目のスライドの内容 */}
-            {/* <div className="flex gap-1">
-                  <div className="flex h-full flex-col justify-start">
-                    <Theme>
-                      <Blockquote size="8">DB情報の連携</Blockquote>
-                    </Theme>
-                    <p className="mt-5">
-                      コラボプロジェクトのデータベース情報を取得し、自動でデータを更新します。
-                    </p>
-                    <p className="mt-5">Firebaseを使用しています。</p>
-                    <p className="mt-5">Next.js上でapiの実装を行っています。</p>
-                  </div>
-                  <div className="rounded-2xl border-2">
-                    <Image
-                      src="/sources/sm_03.png"
-                      alt=""
-                      height={600}
-                      width={280}
-                      style={{ objectFit: "contain", borderRadius: "16px" }}
-                    />
-                  </div>
-                </div> */}
+          </BoardInner>,
+          <BoardInner key="slide3">
             <Theme>
-              <div className="grid h-[469px] w-[669px] grid-cols-2 grid-rows-2 gap-4 rounded-lg p-2">
-                {data.map((item, index) => (
-                  <section
-                    key={index}
-                    className="flex h-full w-full flex-col items-center rounded-xl border-2 border-gray-300 p-2 shadow-lg"
-                  >
-                    {item.icon}
-                    <p className="text-center text-3xl font-extrabold">
-                      {item.title}
-                    </p>
-                    <p className="mt-3">{item.text}</p>
-                  </section>
-                ))}
-              </div>
+              <CardLayout data={data_3} />
             </Theme>
-          </div>,
-          <div key="slide4">
+          </BoardInner>,
+          <BoardInner key="slide4">
             <Theme>
-              <div className="grid h-[469px] w-[669px] grid-cols-2 grid-rows-2 gap-4 rounded-lg p-2">
-                {data_4.map((item, index) => (
-                  <section
-                    key={index}
-                    className="flex h-full w-full flex-col items-center rounded-xl border-2 border-gray-300 p-2 shadow-lg"
-                  >
-                    <div className="relative flex aspect-square w-[100px] items-center justify-center">
-                      <Image
-                        src={item.src}
-                        alt=""
-                        height={100}
-                        width={100}
-                        style={{ objectFit: "cover" }}
-                      />
-                    </div>
-                    <p className="text-center text-3xl font-extrabold">
-                      {item.title}
-                    </p>
-                    <p className="mt-3">{item.text}</p>
-                  </section>
-                ))}
-              </div>
+              <CardLayout data={data_4} />
             </Theme>
-          </div>,
+          </BoardInner>,
         ]}
       />
     </Board>
